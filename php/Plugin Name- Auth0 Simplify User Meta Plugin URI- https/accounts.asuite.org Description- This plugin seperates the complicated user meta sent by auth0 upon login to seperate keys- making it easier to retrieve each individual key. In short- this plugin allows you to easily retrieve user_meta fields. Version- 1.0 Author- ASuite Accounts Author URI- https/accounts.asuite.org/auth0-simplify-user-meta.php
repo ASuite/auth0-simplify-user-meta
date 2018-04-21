85e3e11468af7208869be448900f2621dc0e40d1
WillburHolt <waguilar@rudolfschool.com>
2018-04-21 17:17:49 -0500

    Update and rename auto-update.php to php/Plugin Name- Auth0 Simplify User Meta Plugin URI- https/accounts.asuite.org Description- This plugin seperates the complicated user meta sent by auth0 upon login to seperate keys- making it easier to retrieve each individual key. In short- this plugin allows you to easily retrieve user_meta fields. Version- 1.0 Author- ASuite Accounts Author URI- https/accounts.asuite.org/auth0-simplify-user-meta.php

--- php/Plugin Name- Auth0 Simplify User Meta Plugin URI- https/accounts.asuite.org Description- This plugin seperates the complicated user meta sent by auth0 upon login to seperate keys- making it easier to retrieve each individual key. In short- this plugin allows you to easily retrieve user_meta fields. Version- 1.0 Author- ASuite Accounts Author URI- https/accounts.asuite.org/auth0-simplify-user-meta.php
+++ php/Plugin Name- Auth0 Simplify User Meta Plugin URI- https/accounts.asuite.org Description- This plugin seperates the complicated user meta sent by auth0 upon login to seperate keys- making it easier to retrieve each individual key. In short- this plugin allows you to easily retrieve user_meta fields. Version- 1.0 Author- ASuite Accounts Author URI- https/accounts.asuite.org/auth0-simplify-user-meta.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Auth0 Auto-Update User Meta
+Plugin Name: Auth0 Simplify User Meta
 Plugin URI: https://accounts.asuite.org
 Description: This plugin seperates the complicated user meta sent by auth0 upon login to seperate keys, making it easier to retrieve each individual key. In short, this plugin allows you to easily retrieve user_meta fields.
 Version: 1.0
